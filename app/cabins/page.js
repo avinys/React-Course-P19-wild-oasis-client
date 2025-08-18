8f046517e79b4063091a1b10e0ce74958d49fba0
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-18 12:53:33 +0300

    Add suspence for custom loading indicator displaying in cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,12 @@
-import CabinCard from "@/app/cabins/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "./CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
 	title: "Cabins",
 };
 
 export default async function Page() {
-	console.log("Start");
-	const cabins = await getCabins();
-	console.log(cabins);
-
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,14 +20,9 @@ export default async function Page() {
 				nature&apos;s beauty in your own little home away from home. The
 				perfect spot for a peaceful, calm vacation. Welcome to paradise.
 			</p>
-
-			{cabins.length > 0 && (
-				<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-					{cabins.map((cabin) => (
-						<CabinCard cabin={cabin} key={cabin.id} />
-					))}
-				</div>
-			)}
+			<Suspense fallback={<Spinner />}>
+				<CabinList />
+			</Suspense>
 		</div>
 	);
 }
