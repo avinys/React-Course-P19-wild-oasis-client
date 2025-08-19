e941f31cd6d0055c7e6b7c6c6e83cb33d7e8b455
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-20 00:11:00 +0300

    Add user session fetching and add conditonal redering in reservation making

--- app/cabins/[cabinId]/Reservation.js
+++ app/cabins/[cabinId]/Reservation.js
@@ -1,20 +1,27 @@
 import DateSelector from "@/app/_components/DateSelector";
-import ReservationForm from "./ReservationForm";
+import { auth } from "@/app/_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
+import LoginMessage from "./LoginMessage";
+import ReservationForm from "./ReservationForm";
 
 async function Reservation({ cabin }) {
 	const [settings, bookedDates] = await Promise.all([
 		getSettings(),
 		getBookedDatesByCabinId(cabin.id),
 	]);
+	const session = await auth();
 	return (
-		<div className="grid grid-cols-2 border border-primary-800 min-h-[400px] mb-10">
+		<div className="grid grid-cols-2 border border-primary-800 min-h-[400px] mt-10">
 			<DateSelector
 				settings={settings}
 				bookedDates={bookedDates}
 				cabin={cabin}
 			/>
-			<ReservationForm cabin={cabin} />
+			{session?.user ? (
+				<ReservationForm cabin={cabin} user={session.user} />
+			) : (
+				<LoginMessage />
+			)}
 		</div>
 	);
 }
