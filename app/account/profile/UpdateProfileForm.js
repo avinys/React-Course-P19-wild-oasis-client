76424193b6e277a4b7f97d79e79445e3907d059d
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-20 13:45:26 +0300

    Add profile information update loading indicator using useFormStatus hook

--- app/account/profile/UpdateProfileForm.js
+++ app/account/profile/UpdateProfileForm.js
@@ -1,10 +1,9 @@
 "use client";
 
 import { updateProfile } from "@/app/_lib/actions";
-import { useState } from "react";
+import Button from "./Button";
 
 function UpdateProfileForm({ children, guest }) {
-	const [count, setCount] = useState();
 	const { fullName, email, nationality, nationalID, countryFlag } = guest;
 	return (
 		<form
@@ -53,9 +52,7 @@ function UpdateProfileForm({ children, guest }) {
 			</div>
 
 			<div className="flex justify-end items-center gap-6">
-				<button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-					Update profile
-				</button>
+				<Button />
 			</div>
 		</form>
 	);
