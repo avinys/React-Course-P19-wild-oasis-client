9f2bcc6e8e9a8c9348ca8dafbf303462acb6eedf
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-21 15:21:15 +0300

    Add useOptimistic hook for reservation deletion for better UX and copy paste loading.js file to account for the loading state display

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,6 +1,6 @@
-import { getBookedDatesByCabinId, getBookings } from "@/app/_lib/data-service";
-import ReservationCard from "./ReservationCard";
 import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
+import ReservationList from "./ReservationList";
 
 export const metadata = {
 	title: "Reservations",
@@ -24,11 +24,7 @@ export default async function Page() {
 					</a>
 				</p>
 			) : (
-				<ul className="space-y-6">
-					{bookings.map((booking) => (
-						<ReservationCard booking={booking} key={booking.id} />
-					))}
-				</ul>
+				<ReservationList bookings={bookings} />
 			)}
 		</div>
 	);
